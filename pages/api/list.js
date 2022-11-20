e48106738c126ba35c05b75766d471d93e90e2da
bittnkr <bittnkr@gmail.com>
2022-11-20 11:02:07 -0300

    +singleton database connection

---
+++ pages/api/list.js
@@ -0,0 +1,32 @@
+// import { MongoClient } from "mongodb";
+import connectToDatabase from "../../lib/connectToDatabase";
+
+console.log(connectToDatabase)
+
+export default async function handler(request, response) {
+  try {
+    // console.log('list*')
+    const mongoClient = await connectToDatabase()
+    // console.log(mongoClient)
+
+    const db = mongoClient.db('sample_restaurants')
+
+    const collection = db.collection('restaurants')
+
+    const results = await collection
+      .find({})
+      .project({
+        grades: 0,
+        borough: 0,
+        restaurant_id: 0,
+      })
+      .limit(8)
+      .toArray()
+
+    response.status(200).json(results)
+
+  } catch (e) {
+    console.error(e)
+    response.status(500).json(e)
+  }
+}
