68033b187fb8a102ce67172c9fa24b6061a8b13c
bittnkr <bittnkr@gmail.com>
2022-11-20 18:10:23 -0300

    querying mongodb restaurants

--- pages/sobre.tsx
+++ pages/sobre.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 // import Button from '../components/Button';
 import Contador from '../components/Contador';
+import styles from '../styles/Home.module.css'
 
 function Sobre() {
   return (
-    <>
+    <div className={styles.container}>
       <h1>Sobre</h1>
       {/* <Button /> */}
       <Contador />
       <Link href="/"> Home </Link>
-    </>
+    </div>
   )
 }
 
